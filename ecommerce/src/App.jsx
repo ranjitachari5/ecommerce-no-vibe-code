88526c7c281c1389f81604d35f354c4cfcc40e45
ranjithachari05 <ranjitkumarachari5@gmail.com>
2026-07-21 18:02:30 +0530

    Added Pages and Product component

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
+import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Men from './pages/Men'
 import Banner from './components/Banner'
+import Streetwear from './pages/Streetwear'
+import { Ban } from 'lucide-react'
 function App() {
+  //routes
+ const router = createBrowserRouter([
+  {
+    element: <><Navbar/><Outlet/></>,  
+    children: [
+      { path: "/", element: <Banner/> },
+      {path:"/men", element:<Men/>},
+      { path: "/streetwear", element: <Streetwear/> },
+    ]
+  },
+])
   return (
     <>
-    <Navbar/>
-    <Banner/>
+    <RouterProvider router={router}/>
     </>
   )
 }
 
-export default App
+export default App;
--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -7,51 +7,71 @@ import {
   ShoppingCart,
 } from "lucide-react";
 import { useState } from "react";
-const links = ["New", "Men", "Women", "Streetwear", "Sale"];
-const icons = [
-  <Search />,
-  <User />,
-  <CircleUserRound />,
-  <Heart />,
-  <ShoppingCart />,
-];
+import { Link } from "react-router-dom";//react router dom  
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  // links array with routes
+  const links = [
+  { name: "Home", path: "/" },
+  { name: "Men", path: "/men" },
+  { name: "Women", path: "/women" },
+  { name: "Streetwear", path: "/streetwear" },
+  { name: "Sale", path: "/sale" },
+];
+const icons=[
+  {icon:<User />,path:"/User"},
+  {icon:<CircleUserRound />,path:"/circleuser"},
+  {icon:<Heart />,path:"/wishlist"},
+  {icon:<ShoppingCart />,path:"/shoppingcart"},
+]
   return (
     <>
-    
       <nav className="flex font-serif">
         <div className="flex items-center">
-          <Menu className="block md:hidden cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
-          <p className=" text-3xl text-left p-5 md:text-7xl ">Urban Echo</p>
+          <Menu
+            className="block md:hidden cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          <p className=" text-5xl text-left p-3 md:text-6xl md:text-center  "style={{ fontSize: "clamp(2rem, 5vw, 4.5rem)"}}>Urban Echo</p>
         </div>
         <div className="flex justify-center items-center gap-10">
           <div>
-            <ul className="hidden md:flex gap-3 text-3xl md:pl-20  md:gap-5">
+            <ul className="hidden md:flex gap-3 text-3xl md:pl-20 md:gap-8">
               {links.map((link) => (
-                <li key={link}>
-                  <a
-                    href="/"
-                    className="hover:text-blue-600 transition-colors duration-200"
+                <li key={link.name}>
+                  <Link
+                    to={link.path}
+                    className="cursor-pointer hover:text-blue-600 transition-colors duration-200"
                   >
-                    {link}
-                  </a>
+                    {link.name}
+                  </Link>
                 </li>
               ))}
             </ul>
           </div>
 
-          <div className=" flex gap-3 pl-20  md:gap-5 ">
-            {icons.map((icon) => (
-              <a 
-                href="/"
-                className="cursor-pointer hover:text-blue-500 transition-colors duration-200"
+          <div className=" flex gap-3 pl-20 md:gap-5 ">
+            <div className="flex md:ml-3">
+              <input
+                type="search"
+                placeholder="Search..."
+                className="h-7 w-full hidden  md:flex md:rounded md:border md:border-input md:bg-transparent px-10 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+              <Search className="cursor-pointer hover:text-blue-500 transition-colors md:h-6 md:w-20 " />
+              </div>
+              <div className="flex gap-3 mr-5">
+            {icons.map((icon,index) => (
+              <Link
+              key={index}
+              to={icon.path}
+              className="cursor-pointer hover:text-blue-500 transition-colors duration-200"
               >
-                {icon}
-              </a>
+                {icon.icon}
+              </Link>
             ))}
+            </div>
           </div>
-          </div>
+        </div>
       </nav>
     </>
   );
